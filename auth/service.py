f9f2fd8e5c3ea00bb79c39c14ee34476016d06fb
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-14 16:26:34 +0100

    Update service.py

--- auth/service.py
+++ auth/service.py
@@ -1,4 +1,5 @@
 from db.models import User
+from db.models import Wallet
 from core.security import hash_password, verify_password, create_access_token
 
 def create_user(db, user_data):
@@ -29,3 +30,26 @@ def authenticate_user(db, username, password):
 
 def login_token(user):
     return create_access_token({"sub": str(user.id), "role": user.role})
+
+
+def create_user(db, user_data):
+    user = User(
+        full_name=user_data["full_name"],
+        username=user_data["username"],
+        phone=user_data["phone"],
+        password=hash_password(user_data["password"]),
+        state=user_data["state"],
+        lga=user_data["lga"],
+        role="buyer"
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    # AUTO CREATE WALLET
+    wallet = Wallet(user_id=user.id, balance=0.0)
+    db.add(wallet)
+    db.commit()
+
+    return user
