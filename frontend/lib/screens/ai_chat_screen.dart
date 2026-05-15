7ddb803574d6a69c5cfd7a00d198f7620e10afff
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-15 11:47:01 +0100

    Update ai_chat_screen.dart

--- frontend/lib/screens/ai_chat_screen.dart
+++ frontend/lib/screens/ai_chat_screen.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
 
-class AIChatScreen extends StatelessWidget {
+class AIChatScreen extends StatefulWidget {
+  @override
+  State<AIChatScreen> createState() => _AIChatScreenState();
+}
+
+class _AIChatScreenState extends State<AIChatScreen> {
+  List<String> messages = [];
+  TextEditingController controller = TextEditingController();
+
+  void sendMessage(String msg) {
+    setState(() {
+      messages.add("You: $msg");
+    });
+
+    controller.clear();
+
+    // TODO: send to backend AI later
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Bushmarket AI")),
       body: Column(
         children: [
-          Expanded(child: Container()),
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(messages[index]),
+                );
+              },
+            ),
+          ),
+
           Padding(
             padding: EdgeInsets.all(10),
             child: TextField(
+              controller: controller,
+              onSubmitted: sendMessage,
               decoration: InputDecoration(
                 hintText: "Ask Bushmarket AI...",
+                border: OutlineInputBorder(),
               ),
             ),
           )
@@ -21,12 +52,3 @@ class AIChatScreen extends StatelessWidget {
     );
   }
 }
-
-
-
-TextField(
-  onSubmitted: (msg) {
-    sendToBackend(msg);
-  },
-  decoration: InputDecoration(hintText: "Ask Bushmarket AI"),
-)
