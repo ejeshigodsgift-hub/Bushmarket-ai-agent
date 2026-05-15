fa63f73f4cb1ee8a7e1f48ccacecba3116c98da8
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-15 11:43:55 +0100

    Update local_storage.dart

--- frontend/lib/services/local_storage.dart
+++ frontend/lib/services/local_storage.dart
@@ -1,13 +1,21 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalStorage {
-  static saveToken(String token) async {
+  // SAVE TOKEN
+  static Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString("token", token);
   }
 
+  // GET TOKEN
   static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString("token");
+  }
+
+  // REMOVE TOKEN (LOGOUT)
+  static Future<void> removeToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove("token");
   }
 }
