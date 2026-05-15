6058a6a195d43208ab15d3a9d7ef6d9229d6f822
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-15 12:36:37 +0100

    Update app_routes.dart

--- frontend/lib/routes/app_routes.dart
+++ frontend/lib/routes/app_routes.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'screens/market_screen.dart';
-import 'screens/cooperative_screen.dart';
-import 'screens/wallet_screen.dart';
-import 'screens/ai_chat_screen.dart';
+
+import '../screens/home_screen.dart';
+import '../screens/market_gallery_screen.dart';
+import '../screens/cooperative_screen.dart';
+import '../screens/wallet_screen.dart';
+import '../screens/ai_chat_screen.dart';
 
 class AppRoutes {
   static Map<String, WidgetBuilder> routes = {
     "/home": (context) => HomeScreen(),
-    "/market": (context) => MarketScreen(),
+    "/market": (context) => MarketGalleryScreen(),
     "/coop": (context) => CooperativeScreen(),
     "/wallet": (context) => WalletScreen(),
     "/ai": (context) => AIChatScreen(),
