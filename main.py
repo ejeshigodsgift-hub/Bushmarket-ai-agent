e936e20932c73f7f3543f5dd6dfb68e2db175137
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-17 02:54:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from db.database import Base, engine
 from auth.routes import router as auth_router
 from users.routes import router as user_router
 
+from admin.product_admin import router as product_admin_router
+app.include_router(product_admin_router, prefix="/admin")
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Bushmarket System")
